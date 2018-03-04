23573ac7ede3f550a5128d0da731cb16ff9360bf
Vicente Plata <xientyto@gmail.com>
2018-03-04 11:48:47 -0800

    Documentation

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,13 +1,24 @@
+##
+# Declares links-related route handling routines
+#
 class LinksController < ApplicationController
 
     ##
-    # +GET+
+    # Handles +GET+ requests. Uses the request path as the shortened attribute,
+    # then redirects to the corresponding URL if found.
+    #
+    # Possible HTTP codes:
+    #
+    # [302] Redirection to the appropriate URL
+    # [404] That shortened link was not found
+    # [500] Service derped
     #
     def show
         path = request.env['PATH_INFO']
         # Remove slash
         path = path[1..-1]
         link = Link.find_by(shortened: path)
+
         # Not found
         unless link
             render status: 404
@@ -19,28 +30,36 @@ class LinksController < ApplicationController
     end
 
     ##
-    # +POST+
+    # Handles +POST+ requests. If things went OK, returns a JSON representation of the newly
+    # created object (including the assigned shortened URL). 
+    #
+    # Possible HTTP codes:
+    #
+    # [200] It went _okay_
+    # [415] Something was on the caller's side. Maybe the URL is invalid
+    # [500] Service derped
     #
     def create
-        status = 200
-
         # Create
         link = do_create
 
         # Invalid creation
-        unless link.valid? 
-            status = 415
-        end
-
+        status = ( link.valid? ? 200 : 415)
         json_response(link, status)
     end
 
     private
 
+    ##
+    # Permited parameters
+    #
     def links_params
-        params.permit(:original, :shortened)
+        params.permit(:original)
     end
 
+    ##
+    # Performs the link creation per se
+    #
     def do_create
         link = Link.new
         link.original = links_params[:original]
--- app/models/link.rb
+++ app/models/link.rb
@@ -4,13 +4,19 @@
 class Link < ApplicationRecord
     validates :shortened, presence: true
     validates :original, presence: true, url: true
+
     ##
-    # Creates a short, pseudo-unique string
+    # Creates a short, pseudo-unique string. If for any
+    # reason the string is not 8 characters long (rare, but
+    # I've seen it happening), then underscores are appended
     #
     def self.create_shortened
-        rand(36**8).to_s(36)
+        rand(36**8).to_s(36).rjust(8, '_')
     end
 
+    ##
+    # Exposes the +original+ property
+    #
     def self.get_original
         self.original
     end
